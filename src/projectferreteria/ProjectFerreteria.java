d97ca920e150492564be9a688cbf1cb26a4aa072
BrayamQR <brayam2407@gmail.com>
2022-10-26 23:09:48 -0500

    Ferreteria

---
+++ src/projectferreteria/ProjectFerreteria.java
@@ -0,0 +1,20 @@
+package projectferreteria;
+
+import Form.Login;
+
+/**
+ *
+ * @author BrayamQR
+ */
+public class ProjectFerreteria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login init = new Login();
+        init.setVisible(true);
+    }
+    
+}
